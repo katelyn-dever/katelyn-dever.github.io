eb2cdab872a88a47539a784c38c651c5b817281b
katelyn <48328061+katelyn-dever@users.noreply.github.com>
2019-07-30 15:21:21 -0700

    Portfolio

---
+++ 4-cleaner-oceans/auth.js
@@ -0,0 +1,122 @@
+
+//add admin cloud function
+const adminForm = document.querySelector('.adminForm')
+adminForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const adminEmail = document.querySelector('#inputAdminEmail').value;
+	const addAdminRole = functions.httpsCallable('addAdminRole');
+	addAdminRole({ email: adminEmail }).then(result => {
+		console.log(result);
+		adminForm.reset();
+		$('#adminModal').modal('hide');
+	})
+})
+
+//get today's date to use to limit events to the future
+var today = new Date();
+var dd = String(today.getDate()).padStart(2,'0');
+var mm = String(today.getMonth()+1).padStart(2, '0');
+var yyyy = today.getFullYear();
+today = mm + dd + yyyy;
+
+//get event detail records from database
+db.collection('events').where('id', '>=', today).get().then(snapshot => {
+	getEvents(snapshot.docs);
+});
+
+
+// listen for auth status changes
+auth.onAuthStateChanged(user => {
+	if (user) {
+		user.getIdTokenResult().then(idTokenResult => {
+			user.admin = idTokenResult.claims.admin;
+			setupUI(user);
+		})
+		console.log("user logged in: ", user);
+		
+	} else {
+		console.log("user logged out");
+		setupUI();
+	}
+});
+
+
+// signup
+const signupForm = document.querySelector("#signupForm");
+
+signupForm.addEventListener("submit", (e)=>{
+	e.preventDefault();
+
+	//get user info
+	const email = signupForm["inputEmail"].value;
+	const password = signupForm["inputPassword"].value;
+
+	//sign up the user
+	auth.createUserWithEmailAndPassword(email, password).then(cred => {
+		return db.collection('users').doc(cred.user.uid).set({
+			firstName: signupForm["inputFirstName"].value,
+			lastName: signupForm["inputLastName"].value,
+			email: signupForm["inputEmail"].value
+			});	
+		}).then(() => {
+			signupForm.reset();
+			confirm("Your account has been created!");
+	});
+	
+});
+
+// new event form (from UI)
+const eventForm = document.querySelector("#eventForm");
+
+eventForm.addEventListener("submit", (e)=>{
+	e.preventDefault();
+
+	//get user info
+	const eid = eventForm["inputEID"].value;
+	const date = eventForm["inputDate"].value;
+	const city = eventForm["inputCity"].value;
+	const location = eventForm["inputLocation"].value;
+	const supervisor = eventForm["inputSupervisor"].value;
+	const time = eventForm["inputTime"].value;
+
+	//saves data to database
+	db.collection("events").add({
+		id: eventForm.eid.value,
+		date: eventForm.date.value,
+		city: eventForm.city.value,
+		location: eventForm.location.value,
+		supervisor: eventForm.supervisor.value,
+		time: eventForm.time.value,
+		
+	});
+		eventForm.reset();
+		$('#eventModal').modal('hide');
+});
+
+
+// logout
+const logout = document.querySelector('#logout');
+logout.addEventListener("click", (e) => {
+	e.preventDefault();
+	auth.signOut();
+});
+
+
+// login
+const loginForm = document.querySelector('.loginform');
+loginForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	// get user info
+	const email = loginForm["loginEmail"].value;
+	const password = loginForm["loginPassword"].value;
+
+	auth.signInWithEmailAndPassword(email, password).then(cred => {
+		loginForm.reset();
+	});
+});
+
+
+
+
+
---
+++ cabin-club/README.md
@@ -0,0 +1,5 @@
+# WebDevFinal
+Cabin Club Website
+
+Project created for a final in Web Development course
+Fall 2018
